cc3a94dcfc5eaf6c255876670b8afb280a58b805
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-11-24 17:56:55 -0300

    chore: some tests in main class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import structures.MinHeap;
+import structures.Vector;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Vector rawHeap = new Vector(10);
+        rawHeap.add(20, 33, 2, 1, 8, 3, 14);
+
+        System.out.println("Original raw 'heap' ");
+        System.out.println(rawHeap);
+
+        MinHeap minHeap = new MinHeap(rawHeap);
+
+        System.out.println("Converting a raw heap to a min heap");
+        System.out.println(minHeap);
+
+        int minimum = minHeap.poll();
+
+        System.out.println("By removing the minimum element: " + minimum + ", we will have the new minimum heap:");
+        System.out.println(minHeap);
+
     }
 }
